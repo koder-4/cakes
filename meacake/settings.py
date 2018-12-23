ce8e8f12d2d0df51c52ad854ceff4f19566e85f2
Koder Sohm <k0der85@mail.ru>
2018-12-23 10:47:09 +0300

    debug = False

--- meacake/settings.py
+++ meacake/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2&usy4x0va)x((=q&u2m2mi7r-#1(plp%2m9)r8meqlgww#o&&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
